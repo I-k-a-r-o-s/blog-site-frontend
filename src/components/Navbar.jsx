72af7ddf3c15cb9cdfba0e1101af7c7ad11d9c02
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-05-21 11:03:24 +0530

    base navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { BiLogIn } from "react-icons/bi";
+import logo from "../assets/logo.jpeg";
+import { useNavigate } from "react-router";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="navbar bg-base-300 shadow-sm py-5">
+      <div className="navbar-start">
+        <button
+          className="btn btn-ghost text-2xl gap-5"
+          onClick={() => navigate("/")}
+        >
+          <img src={logo} alt="logo" className="w-15 h-15 rounded-full" />
+          Blog
+        </button>
+      </div>
+      <div className="navbar-end">
+        <button
+          className="btn btn-primary rounded-full gap-1"
+          onClick={() => navigate("/admin")}
+        >
+          Login
+          <BiLogIn size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
