cd0635b9b8bcfb9bf9d4edab64ba5adf82f92e5d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-05-25 17:01:50 +0530

    added the background to the whole app.fixed the wrong route path for <Blog/> element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import ParticleBackground from "./components/ParticleBackground";
 
 const App = () => {
   return (
     <div>
+      <ParticleBackground/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<Blog />} />
       </Routes>
     </div>
   );
