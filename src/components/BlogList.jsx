4f8b168947114e4281daf2592f47c2e8864762a9
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-05-25 17:04:09 +0530

    added blog card with the blog list

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { motion } from "motion/react";
+import BlogCard from "./BlogCard";
+import { blog_data } from "../assets/assets";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
@@ -15,7 +17,9 @@ const BlogList = () => {
       <div className="flex justify-center gap-4 sm:gap-8 my-10 reactive">
         {blogCategories.map((item) => (
           <div key={item} className="relative">
-            <motion.button animate={{ rotate: 360 }} transition={{ duration: 1 }}
+            <motion.button
+              animate={{ rotate: 360 }}
+              transition={{ duration: 1 }}
               className={`btn ${menu === item ? "btn-primary" : "btn-outline btn-primary"}`}
               onClick={() => setMenu(item)}
             >
@@ -24,7 +28,13 @@ const BlogList = () => {
           </div>
         ))}
       </div>
-      <div className="">{/*blog cards*/}</div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data
+          .filter((blog) => (menu === "All" ? true : menu === blog.category))
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
+      </div>
     </div>
   );
 };
