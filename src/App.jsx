befd61e447a95ec67514d2a26f05ab40f7210464
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-05-26 15:16:24 +0530

    removed navbar from home.jsx and added to app.jsx for the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import ParticleBackground from "./components/ParticleBackground";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <ParticleBackground/>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ import Footer from "../components/Footer";
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Header />
       <BlogList/>
       <Newsletter/>
