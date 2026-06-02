c390ef5cd034c22643603b24cde92572e0b54ae1
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-02 16:30:21 +0530

    wrote addBlogPost page. refined styling is required

--- src/pages/admin/AddBlogPost.jsx
+++ src/pages/admin/AddBlogPost.jsx
@@ -1,6 +1,128 @@
+import { useEffect, useRef, useState } from "react";
+import { BsCloudUpload } from "react-icons/bs";
+import Quill from "quill";
+
 const AddBlogPost = () => {
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [category, setCategory] = useState("StartuP");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const blogCategories = [
+    "All",
+    "Technology",
+    "Startup",
+    "Lifestyle",
+    "Finance",
+  ];
+
+  const handleSubmit = async (e) => {
+    e.preventDefauult();
+  };
+
+  const generateContent = async () => {};
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, { theme: "snow" });
+    }
+  }, []);
   return (
-    <div>AddBlogPost</div>
-  )
-}
-export default AddBlogPost
+    <div>
+      <h1 className="text-2xl text-primary pb-5 text-center">Add Blog Post</h1>
+      <form onSubmit={handleSubmit} className="flex-1 h-full overflow-scroll">
+        <div className="bg-base-200 w-full max-w-3xl p-4 md:p-10 sm:m-10 rounded">
+          <p>Upload Thumbnail</p>
+          <label className="label">
+            {!image ? (
+              <>
+                <BsCloudUpload
+                  size={30}
+                  className="mt-2 mx-12 cursor-pointer"
+                />
+                <input
+                  type="file"
+                  id="image"
+                  hidden
+                  required
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+              </>
+            ) : (
+              <>
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt="image"
+                  className="mx-8 mt-2 w-15 h-15 rounded"
+                />
+              </>
+            )}
+          </label>
+          <p className="mt-4 mb-2">Blog Title</p>
+          <input
+            type="text"
+            className="input"
+            placeholder="Title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <p className="mt-4 mb-2">Subtitle</p>
+          <input
+            type="text"
+            className="input"
+            placeholder="Title"
+            required
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+
+          <p className="mt-4 mb-2">Blog Description</p>
+          <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
+            <div ref={editorRef}></div>
+            <button
+              className="btn btn-secondary absolute bottom-1 right-2 ml-2 px-4 py-1.5"
+              onClick={generateContent}
+            >
+              Generate with AI
+            </button>
+          </div>
+
+          <p className="mt-4 mb-2">Blog Category</p>
+          <select
+            className="select"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="" hidden>
+              Select Category
+            </option>
+            {blogCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <div className="flex gap-2 mt-4">
+            <p>Publish Now</p>
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={isPublished}
+              onChange={(e) => setIsPublished(e.target.chcked)}
+            />
+          </div>
+
+          <button className="btn btn-primary mt-8" type="submit">
+            Add Blog
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default AddBlogPost;
