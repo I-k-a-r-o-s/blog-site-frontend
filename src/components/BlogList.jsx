f2191d60ec16dfb6df11b8da9ad459e13fa54abe
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-08 18:54:40 +0530

    added blog data displaying according to filters. no blogs fall back to NoBlog component

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { motion } from "motion/react";
 import BlogCard from "./BlogCard";
-import { blog_data } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import NoBlogs from "./NoBlogs";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
+
+  const { blogs, input } = useAppContext();
+
   const blogCategories = [
     "All",
     "Technology",
@@ -12,6 +16,17 @@ const BlogList = () => {
     "Lifestyle",
     "Finance",
   ];
+
+  const filterBlogs = () => {
+    if (input === "") {
+      return blogs;
+    }
+    return blogs.filter(
+      (blog) =>
+        blog.title.toLowerCase().includes(input.toLowerCase()) ||
+        blog.category.toLowerCase().includes(input.toLowerCase()),
+    );
+  };
   return (
     <div>
       <div className="flex justify-center gap-4 sm:gap-8 my-10 reactive">
@@ -28,12 +43,17 @@ const BlogList = () => {
           </div>
         ))}
       </div>
+
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mb-24 mx-8 sm:mx-16 xl:mx-40">
-        {blog_data
-          .filter((blog) => (menu === "All" ? true : menu === blog.category))
-          .map((blog) => (
-            <BlogCard key={blog._id} blog={blog} />
-          ))}
+        {filterBlogs().filter((blog) =>
+          menu === "All" ? true : menu === blog.category,
+        ).length === 0 ? (
+          <NoBlogs category={menu} />
+        ) : (
+          filterBlogs()
+            .filter((blog) => (menu === "All" ? true : menu === blog.category))
+            .map((blog) => <BlogCard key={blog._id} blog={blog} />)
+        )}
       </div>
     </div>
   );
