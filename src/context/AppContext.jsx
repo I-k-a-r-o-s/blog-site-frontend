ba4f782c340d99727d6a2c8ab1e1a86c91c744ee
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-08 18:00:03 +0530

    fetch blog data from backend function

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,42 @@
-import { createContext, useContext } from "react";
-
-export const appContext = createContext();
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../api/Axios";
+import toast from "react-hot-toast";
+const appContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const appValues = {};
+  const [token, setToken] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [input, setInput] = useState("");
+
+
+  const fetchBlogs = async () => {
+    try {
+      const { data } = await api.get("/api/blog/all");
+      data.success ? setBlogs(data.blogs) : toast.error(data.message);
+    } catch (error) {
+      console.log("Error in fetchBlogs!:", error.response?.data);
+      toast.error(error.response?.data?.message || "Internal Server Error!");
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+
+    const token = localStorage.getItem("token");
+    if (token && !token === null) {
+      setToken(token);
+      api.defaults.headers.common["Authorization"] = `${token}`;
+    }
+  }, []);
+
+  const appValues = {
+    token,
+    setToken,
+    blogs,
+    setBlogs,
+    input,
+    setInput,
+  };
   return (
     <appContext.Provider value={appValues}>{children}</appContext.Provider>
   );
