8f33b9efed5161ba196f18df8c15ed772361d418
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-10 17:02:17 +0530

    added fetchBlogs to the homePage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import BlogList from "../components/BlogList";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { useAppContext } from "../context/AppContext";
 
 const Home = () => {
+  const { fetchBlogs } = useAppContext();
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <>
       <Navbar />
