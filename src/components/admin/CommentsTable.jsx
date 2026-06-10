41ba6a0ada3766d94dc40ed56f651a7be2729b2b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-10 17:35:16 +0530

    plugged in backend

--- src/components/admin/CommentsTable.jsx
+++ src/components/admin/CommentsTable.jsx
@@ -1,9 +1,47 @@
+import toast from "react-hot-toast";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { TiTickOutline } from "react-icons/ti";
+import { api } from "../../api/Axios";
 
 const CommentsTable = ({ comment, fetchComments }) => {
   const { blog, createdAt, _id, content, name, isApproved } = comment;
   const blogDate = new Date(createdAt);
+
+  const approvedStatus = async () => {
+    try {
+      const { data } = await api.post(`/api/admin/approve-comment/${_id}`);
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Internal Server Error!");
+      console.log("Error in approvedStatus!:", error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const confirmDelete = window.confirm("Are you SURE you want to delete?");
+      if (!confirm) {
+        return;
+      }
+
+      const { data } = await api.delete(`/api/admin/delete-comment/${_id}`);
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Internal Server Error!");
+      console.log("Error in handleDelete!:", error);
+    }
+  };
+
   return (
     <tr>
       <td>
@@ -18,13 +56,16 @@ const CommentsTable = ({ comment, fetchComments }) => {
       <td className="px-6 py-4">
         <div className="inline-flex items-center gap-4">
           {!isApproved ? (
-            <button className="btn btn-circle btn-primary">
+            <button
+              className="btn btn-circle btn-primary"
+              onClick={approvedStatus}
+            >
               <TiTickOutline size={20} />
             </button>
           ) : (
             <p className="text-success text-xs px-3 py-1">Approved</p>
           )}
-          <button className="btn btn-circle btn-error">
+          <button className="btn btn-circle btn-error" onClick={handleDelete}>
             <RiDeleteBinLine size={20} />
           </button>
         </div>
